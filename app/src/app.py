787cc78c53764baa3f67f05b238985da131a6851
Zareen <zareenk@outlook.com>
2023-10-08 18:26:36 -0400

    Added basic HTML and CSS files for page

--- app/src/app.py
+++ app/src/app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def index():
+    return render_template('index.html')
