e666e38d627e6e058f8e4b048f59d2db8e800d32
racheldennis <racheldennis1504@gmail.com>
2023-10-29 22:35:40 -0400

    changes made for register and database migrate

--- app/src/app.py
+++ app/src/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
-from .models import db, User
-from .auth import auth as auth_blueprint
+from models import db, User
+from auth import auth as auth_blueprint
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
 
-# Configure and initalize database
+# Create an instance of Flask-Migrate and associate it with your Flask app and database
+migrate = Migrate(app, db)
+
+# Configure and initalize database (need to figure out Google MySQL Cloud)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 login_manager = LoginManager()
