11332b4c3929ef96b10415010252db9fb6aa8baf
Sky Deng <sky.deng.online@outlook.com>
2023-11-18 18:03:15 -0500

    created the frontend for the quiz

--- app/src/main.py
+++ app/src/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .models import Quiz
 
 main = Blueprint('main', __name__)
 
@@ -15,3 +16,24 @@ def profile():
         grade=current_user.grade.value,
         logged_in=True
     )
+    
+@main.route('/quiz/<int:quiz_id>', methods=['GET'])
+@login_required
+def quiz_page(quiz_id):
+    user = current_user
+    quiz = Quiz.query.get(quiz_id)
+
+    user_attempted = quiz in user.quizzes
+
+    # Users highest score
+    highest_score = 0
+    if user_attempted:
+        highest_score = quiz.users.filter_by(id=user.id).first().score
+    
+    # Temporary workaround since there are no quizzes right now, just to prevent an error
+    if not quiz:
+        questions = None
+    else:
+        questions = [(i + 1, question) for i, question in enumerate(quiz.questions)]
+
+    return render_template('quiz.html', quiz=quiz, user_attempted=user_attempted, highest_score=highest_score, questions=questions)
